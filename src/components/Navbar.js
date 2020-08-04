a50451c2abe47311a07d1bbbd7f0286a63ba514b
wm96n2c <wm96n2c@gmail.com>
2020-08-04 11:38:22 -0600

    Navbar works on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,32 @@ import { NavLink } from 'react-router-dom';
 class Navbar extends Component {
     constructor(props) {
         super(props)
+
+        this.toggleNav = this.toggleNav.bind(this)
+
+        this.state = {
+            navExtended: false
+        }
+    }
+
+    toggleNav() {
+        this.state.navExtended ? document.getElementsByClassName('nav')[0].classList.remove('extended') : document.getElementsByClassName('nav')[0].classList.add('extended')
+        this.setState((prev) => ({ navExtended: !prev.navExtended }))
     }
 
     render() {
         return (
-            <div>
-                <nav className="nav">
-                    <h1>weylin.dev</h1>
-                    <NavLink onClick={this.animateNavbar} to="/" activeClassName="nav-active"exact={true} >Home</NavLink>
-                    <NavLink to="/portfolio" activeClassName="nav-active" >Portfolio</NavLink>
-                    {/* <NavLink to="/blog" activeClassName="nav-active" >Blog</NavLink> !! STILL IN DEVELOPMENT!! */}
-                    <NavLink to="/contact" activeClassName="nav-active" >Contact</NavLink>
-                </nav>
+            <div className="nav-animation-container">
+                <div className="nav-container">
+                    <NavLink to="/"><h1>weylin.dev</h1></NavLink>
+                    <nav className="nav">
+                        <NavLink to="/" activeClassName="nav-active"exact={true} >Home</NavLink>
+                        <NavLink to="/portfolio" activeClassName="nav-active" >Portfolio</NavLink>
+                        {/* <NavLink to="/blog" activeClassName="nav-active" >Blog</NavLink> !! STILL IN DEVELOPMENT!! */}
+                        <NavLink to="/contact" activeClassName="nav-active" >Contact</NavLink>
+                    </nav>
+                    <button onClick={this.toggleNav} className="nav-toggle">☰</button>
+                </div>
                 <div className="nav-animation"></div>
             </div>
         );
