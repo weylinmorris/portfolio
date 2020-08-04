378b8058152f3d7909a44072eecf1b639ce33648
wm96n2c <wm96n2c@gmail.com>
2020-08-03 21:03:23 -0600

    Configured for heroku

--- src/server.js
+++ src/server.js
@@ -5,7 +5,11 @@ const app = express()
 
 const publicPath = path.join(__dirname, '../public')
 
-app.use(express.static(publicPath, { extensions: ['html'] }))
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, '/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('App running on port 3000')
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public/'),
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
